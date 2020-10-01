ded8ba660fff2049f4c0fac5a5bec27fd40c2a3b
jagriti24 <jagriti.singh.2407@gmail.com>
2020-10-01 19:50:06 +0530

    Update count-complete-tree-nodes.py

--- leetcode_medium/count-complete-tree-nodes.py
+++ leetcode_medium/count-complete-tree-nodes.py
@@ -1,1 +1,42 @@
+# https://leetcode.com/problems/count-complete-tree-nodes/
+"""
+Given a complete binary tree, count the number of nodes.
 
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input: 
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.count(root)
+        
+    def count(self, node):
+        if node is None:
+            return 0
+        else:
+            return self.count(node.left) + self.count(node.right) + 1
+            
