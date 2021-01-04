be2abb6368746d114ed127d5ae1f2204e0dd1b00
jagriti24 <jagriti.singh.2407@gmail.com>
2021-01-05 00:54:34 +0530

    Create TwoSum.java

---
+++ leetcode_easy/TwoSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0 ; i < nums.length; i++){
+           // if(nums[i]<= target){
+               int diff = target-nums[i];
+               // System.out.println("i: "+i);
+               // System.out.println(diff);
+               for(int j= i+1; j<nums.length; j++){
+                   if(nums[j]==diff){
+                       result[0] = i;
+                       result[1] = j;
+                       return result;
+                   }
+               // }
+           } 
+        }
+        return result;
+    }
+}
